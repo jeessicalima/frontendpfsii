12c22388042e1ad1d824f9173e8091a1dbe3ecd6
Jessica <jeessica.lima@outlook.com>
2023-10-19 19:42:33 -0300

    alteração app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useContext } from "react";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import TelaMenu from "./interfaces/TelaMenuSistema.js";
+import Tela404 from "./interfaces/Tela404.js";
+import TelaCadastroExemplar from "./interfaces/TelaFormExemplar.jsx";
+import TelaDevolucao from "./interfaces/TelaCadastroDevolucao.jsx";
 import AppRoutes from "./AppRoutes.jsx"
 
 function App() {
   return (
     <div className="App">
-      <AppRoutes />
-      {}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/frontendpfsii" element={<TelaMenu></TelaMenu>}></Route>
+          <Route path="/" element={<TelaMenu />}></Route>
+          <Route path="/exemplar" element={<TelaCadastroExemplar/>}></Route>
+          <Route path="/devolucao" element={<TelaDevolucao />}></Route>
+          <Route path="*" element={<Tela404 />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
